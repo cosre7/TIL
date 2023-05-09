fa5b1e5630bf10c6f524184d783ae45878da6a33
cosre7 <cosre7@gmail.com>
2023-05-09 17:31:11 +0900

    javascript module - ES6

---
+++ javaScript/frontend_wing/module/index.js
@@ -0,0 +1,9 @@
+// import {perfectScore, sum, avg} from './math.js';
+//import * as math from './math.js';
+// import subtract2 from './math.js';
+import math from './math.js';
+
+console.log('perfectScore :', math.perfectScore);
+console.log('sum :', math.sum(80, 10));
+console.log('avg :', math.avg(80, 90));
+console.log('subtract :', math.subtract(80, 90));
