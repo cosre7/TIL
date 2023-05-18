9c55922ed5039457d0cbdc05363eafde9459bdbd
cosre7 <cosre7@gmail.com>
2023-05-18 10:53:37 +0900

    [kotlin-class101-정대리] 반환을 가지는 함수

--- kotlin/Functiontutorial/app/src/main/java/com/jessieinapp/functiontutorial/MainActivity.kt
+++ kotlin/Functiontutorial/app/src/main/java/com/jessieinapp/functiontutorial/MainActivity.kt
@@ -15,9 +15,12 @@ import java.util.Timer
 import kotlin.concurrent.schedule
 
 // 함수를 정의한다, 선언한다
-fun sayHello(name: String = "이름없음") {
+fun sayHello(name: String = "이름없음") : Unit {
     println("안녕? 난 ${name}야")
     println("너는 누구니?")
+
+    // 생략 상태
+    return Unit
 }
 
 // 라면을 끓이는 함수
@@ -36,10 +39,19 @@ fun cookRamen(ramenKind: String = "일반") {
 }
 
 // 무작위 수를 말하는 함수
-fun sayRandomNumber(min: Int = 1, max: Int = 999) {
-    val randomNumber = (min..max).random()
-    println("무작위 수는 $randomNumber 입니다!")
-}
+//fun getRandomNumber(min: Int = 1, max: Int = 999) : Int {
+//    // 반환 방법1
+////    val randomNumber = (min..max).random()
+////    return randomNumber
+//
+//    // 반환 방법2
+////    return (min..max).random()
+//}
+// 반환 방법3
+//fun getRandomNumber(min: Int = 1, max: Int = 999) : Int = (min..max).random()
+
+// 반환 방법 4
+fun getRandomNumber(min: Int = 1, max: Int = 999) = (min..max).random()
 
 // 프로그램이 실행되는 메인함수
 fun main() {
@@ -48,9 +60,12 @@ fun main() {
     sayHello("정대리") // name = "정대리"
 //    cookRamen()
 //    cookRamen("짜장")
-    sayRandomNumber() // min = 1, max = 999
-    sayRandomNumber(max = 10) // min = 1, max = 10
-    sayRandomNumber(min = 999) // min = 999, max = 999
+//    sayRandomNumber() // min = 1, max = 999
+//    sayRandomNumber(max = 10) // min = 1, max = 10
+//    sayRandomNumber(min = 999) // min = 999, max = 999
+    val randomNumber = getRandomNumber()
+    println("main() - randomNumber: $randomNumber")
+    println("main() - randomNumber: ${getRandomNumber()}")
 }
 
 class MainActivity : ComponentActivity() {
