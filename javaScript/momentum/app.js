ee743b45f3a17c23725ea6965a66325cd751115b
cosre7 <cosre7@gmail.com>
2023-08-01 18:29:42 +0900

    바닐라JS로 크롬 앱 만들기 - #2.3

--- javaScript/momentum/app.js
+++ javaScript/momentum/app.js
@@ -1,19 +1,11 @@
 const a = 5;
-
-console.log(5 + 2); // 7
-console.log(5 * 2); // 10
-console.log(5 / 2); // 2.5
-
-console.log(a + 2); // 7
-console.log(a * 2); // 10
-console.log(a / 2); // 2.5
-
 const b = 2;
+let myName = 'jessie';
 
 console.log(a + b); // 7
 console.log(a * b); // 10
 console.log(a / b); // 2.5
-
-const myName = 'jessie';
-console.log('Hello my name is ' + 'jessie'); // Hello my name is jessie
 console.log('Hello my name is ' + myName); // Hello my name is jessie
+
+myName = 'you jin';
+console.log('my new name is ' + myName);
