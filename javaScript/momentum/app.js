fd47b3bb4e192e1e84bbc7224602fa89bc18d0d6
cosre7 <cosre7@gmail.com>
2023-08-09 10:27:52 +0900

    learn function_2

--- javaScript/momentum/app.js
+++ javaScript/momentum/app.js
@@ -1,7 +1,22 @@
-function sayHello(nameOfPerson, age) {
-    console.log('hello my name is ' + nameOfPerson + ' and I\'m ' + age);
+function plus(firstNumber, secondNumber) {
+    console.log(firstNumber + secondNumber);
 }
 
-sayHello('nico', 10);
-sayHello('dal', 23);
-sayHello('lynn', 21);
+// firstNumber, secondNumber는 function 안에서만 존재한다
+// console.log(firstNumber); // not defined 에러
+
+function divide(a, b) { 
+    console.log(a / b);
+}
+plus(8, 60);
+divide(98, 20);
+
+const player = {
+    name: 'nico',
+    sayHello: function(otherPersonsName) {
+        console.log('hello ' + otherPersonsName + ' nice to meet you!');
+    }
+}
+
+console.log(player.name);
+player.sayHello('lynn');
