8307313a1adb8f2590105c2de15af15b82139b98
cosre7 <cosre7@gmail.com>
2023-08-11 09:34:29 +0900

    learn clickEvent

--- javaScript/momentum/app.js
+++ javaScript/momentum/app.js
@@ -1,40 +1,14 @@
 const h1 = document.querySelector('.hello h1')
 
 function handleTitleClick() {
-    h1.style.color = 'blue';
-    console.log('title was clicked!');
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor === 'blue') {
+        newColor = 'tomato';
+    } else {
+        newColor = 'blue';
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter() {
-    h1.innerText = 'Mouse is here!';
-}
-
-function handleMouseLeave() {
-    h1.innerText = 'Mouse is gone!';
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = 'tomato';
-}
-
-function handleWindowCopy() {
-    alert('copier!');
-}
-
-function handleWindowOffline() {
-    alert('SOS no WIFI');
-}
-
-function handleWindowOnline() {
-    alert('All Gooood!');
-}
-
-// title.addEventListener('click', handleTitleClick);
 h1.addEventListener('click', handleTitleClick);
-h1.addEventListener('mouseenter', handleMouseEnter);
-h1.addEventListener('mouseleave', handleMouseLeave);
-
-window.addEventListener('resize', handleWindowResize);
-window.addEventListener('copy', handleWindowCopy);
-window.addEventListener('offline', handleWindowOffline);
-window.addEventListener('online', handleWindowOnline);
