250cb0ff2a722fafcef40366ebc97a3320addf17
cosre7 <cosre7@gmail.com>
2023-12-28 17:18:48 +0900

    feat : category 추가

---
+++ NextJS/technews/data.js
@@ -0,0 +1,35 @@
+export const postData = [
+    {
+        id: 1,
+        title: "New Smartphone Model Released",
+        content: "A New smartPhone model with advanced features has been released.",
+        author: "John Doe",
+        datepublished: "2023-09-20",
+        category: "Mobile",
+        links: ["https://example.com/news1"],
+        thumbnail: "https://example.com/thumbnail1.jpg",
+    },
+    {
+        id: 2,
+        title: "New Smartphone Model Released",
+        content: "A New smartPhone model with advanced features has been released.",
+        author: "John Doe",
+        datepublished: "2023-09-20",
+        category: "Mobile",
+        links: ["https://example.com/news1"],
+        thumbnail: "https://example.com/thumbnail1.jpg",
+    }
+];
+
+export const categoriesData = [
+    {
+        id: "1",
+        name: "Technology"
+    },
+    {
+        id: "2",
+        name: "Tips & Tricks"
+    }
+]
+
+
