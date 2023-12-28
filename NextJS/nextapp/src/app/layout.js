de12fcb31b01180ee35d6557ac1d0b2eb7a15321
cosre7 <cosre7@gmail.com>
2023-12-28 10:59:02 +0900

    refactor : a 태그와 link 태그의 차이

--- NextJS/nextapp/src/app/layout.js
+++ NextJS/nextapp/src/app/layout.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { Inter } from 'next/font/google'
 
 export const metadata = {
@@ -9,15 +10,15 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <body>
-        <h1><a href="/">WEB</a></h1>
+        <h1><Link href="/">WEB</Link></h1>
         <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
+          <li><Link href="/read/1">html</Link></li>
+          <li><Link href="/read/2">css</Link></li>
         </ol>
         {children}
         <ul>
-          <li><a href="/create">Create</a></li>
-          <li><a href="/update">Update</a></li>
+          <li><Link href="/create">Create</Link></li>
+          <li><Link href="/update">Update</Link></li>
           <li><input type="button" value="Delete" /></li>
         </ul>
       </body>
