35d4f09056ca7df1beac43ac05feda8259e50539
cosre7 <cosre7@gmail.com>
2023-12-28 15:08:13 +0900

    refactor : create 시 바로 목록 재구성하도록 변경

--- NextJS/nextapp/src/app/create/page.js
+++ NextJS/nextapp/src/app/create/page.js
@@ -22,6 +22,7 @@ export default function Create() {
                     console.log(result);
                     const lastId = result.id;
                     router.push(`/read/${lastId}`);
+                    router.refresh();
                 })
         }}>
             <p>
