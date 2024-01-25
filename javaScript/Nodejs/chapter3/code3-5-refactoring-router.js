0a25fcf99848d02da9e39bcb6ac76bc62d390b7d
cosre7 <cosre7@gmail.com>
2024-01-25 16:02:31 +0900

    refactor : 주석 추가

--- javaScript/Nodejs/chapter3/code3-5-refactoring-router.js
+++ javaScript/Nodejs/chapter3/code3-5-refactoring-router.js
@@ -4,8 +4,8 @@ http.createServer((req, res) => {
     const path = url.parse(req.url, true).pathname;
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
 
-    if (path in urlMap) {
-        urlMap[path](req, res);
+    if (path in urlMap) { // 1) urlMap에 path가 있는지 확인
+        urlMap[path](req, res); // 2) urlMap에 path값으로 매핑된 함수 실행
     } else {
         notFound(req, res);
     }
@@ -34,6 +34,7 @@ const notFound = (req, res) => {
     res.end('404 page not found'); 
 }
 
+// 3) 라우터 규칙 매핑 키로 path가 들어가고 값에 함수를 할당
 const urlMap = {
     '/' : (req, res) => res.end('HOME'),
     '/user': user,
