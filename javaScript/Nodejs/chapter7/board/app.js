224a4f2eecf2479cfbad3fa71db8dfc04b8b0295
cosre7 <cosre7@gmail.com>
2024-02-01 08:40:46 +0900

    refactor : 메인 화면 UI 만들기

--- javaScript/Nodejs/chapter7/board/app.js
+++ javaScript/Nodejs/chapter7/board/app.js
@@ -8,7 +8,7 @@ app.set('views', __dirname + '/views'); // 3) 뷰 디렉터리를 view로 설정
 
 // 4) 라우터 설정
 app.get('/', (req, res) => {
-    res.render('home', {title: '안녕하세요', message: '만나서 반갑습니다!'});
+    res.render('home', {title: '테스트 게시판'});
 });
 
 app.listen(3000);
